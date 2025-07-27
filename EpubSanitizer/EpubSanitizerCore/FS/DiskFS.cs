f19e338746756c73f9abb4dcfd246cb6f486787b
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-07-27 16:31:38 +1000

    [fs] Basic structure for file system

---
+++ EpubSanitizer/EpubSanitizerCore/FS/DiskFS.cs
@@ -0,0 +1,27 @@
+﻿using System.IO.Compression;
+
+namespace EpubSanitizerCore.FS
+{
+    internal class DiskFS : FileSystem
+    {
+        internal override void Export(ZipArchive EpubFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void Import(ZipArchive EpubFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override string Read(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void Write(string path, string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EpubSanitizer/EpubSanitizerCore/FS/FileSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.IO.Compression;
+
+namespace EpubSanitizerCore.FS
+{
+    /// <summary>
+    /// Abstract class of file system used for processing
+    /// </summary>
+    internal abstract class FileSystem
+    {
+        /// <summary>
+        /// Write string content to target path
+        /// </summary>
+        /// <param name="path">relative path</param>
+        /// <param name="content">string content</param>
+        internal abstract void Write(string path, string content);
+        /// <summary>
+        /// Read string content
+        /// </summary>
+        /// <param name="path">relative path</param>
+        /// <returns>string content</returns>
+        internal abstract string Read(string path);
+        // TODO: support non-text processing
+
+        /// <summary>
+        /// Load content from Epub file
+        /// </summary>
+        /// <param name="EpubFile">The original Epub file</param>
+        internal abstract void Import(ZipArchive EpubFile);
+        /// <summary>
+        /// Export content to a new Epub file
+        /// </summary>
+        /// <param name="EpubFile"></param>
+        internal abstract void Export(ZipArchive EpubFile);
+    }
+}
