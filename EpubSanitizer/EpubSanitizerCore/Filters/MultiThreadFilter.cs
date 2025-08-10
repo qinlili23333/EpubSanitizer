97d5542a2283f8501be5954b2919538ff367c4e4
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-08-10 13:04:24 +1000

    [filter] Multi thread already implemented

--- EpubSanitizer/EpubSanitizerCore/Filters/MultiThreadFilter.cs
+++ EpubSanitizer/EpubSanitizerCore/Filters/MultiThreadFilter.cs
@@ -2,7 +2,6 @@
 {
     /// <summary>
     /// Abstract class of multi thread filter, but MT support is not ready so currently just process as single thread
-    /// TODO: implement multi thread
     /// </summary>
     internal abstract class MultiThreadFilter(EpubSanitizer CoreInstance) : SingleThreadFilter(CoreInstance)
     {
