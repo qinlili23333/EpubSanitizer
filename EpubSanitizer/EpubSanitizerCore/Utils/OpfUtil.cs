108117d89b8570bc01c7607b8416847bacedb613
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-08-11 13:45:23 +1000

    [core,util] Fix uid in ncx

---
+++ EpubSanitizer/EpubSanitizerCore/Utils/OpfUtil.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml;
+
+namespace EpubSanitizerCore.Utils
+{
+    internal static class OpfUtil
+    {
+        /// <summary>
+        /// Get unique identifier from OPF document
+        /// </summary>
+        /// <param name="OpfDoc">OPF XmlDocument object</param>
+        /// <returns>unique identifier if exist (required in standard), or random GUID generated</returns>
+        internal static string GetUniqueIdentifier(XmlDocument OpfDoc)
+        {
+            string identifierId = OpfDoc.DocumentElement.GetAttribute("unique-identifier");
+            if (identifierId != string.Empty)
+            {
+                foreach (var item in OpfDoc.GetElementsByTagName("dc:identifier"))
+                {
+                    if (item is XmlElement itemEle)
+                    {
+                        if (itemEle.GetAttribute("id") == identifierId)
+                        {
+                            return itemEle.InnerText;
+                        }
+                    }
+                }
+            }
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
