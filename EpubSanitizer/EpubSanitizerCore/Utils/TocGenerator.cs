9cd5fab69f4d6b028cd6051a5d7ff9f07b0fdc7b
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-08-16 12:18:22 +1000

    [core] Add TocXhtml template

--- EpubSanitizer/EpubSanitizerCore/Utils/TocGenerator.cs
+++ EpubSanitizer/EpubSanitizerCore/Utils/TocGenerator.cs
@@ -5,12 +5,25 @@ namespace EpubSanitizerCore.Utils
     internal static class TocGenerator
     {
         /// <summary>
+        /// Empty template, loaded at static constructor.
+        /// </summary>
+        private readonly static XmlDocument emptyNavDoc = new();
+        /// <summary>
+        /// Parse string template for nav.xhtml document.
+        /// Only do once and cached in static field to accelerate for batch processing.
+        /// </summary>
+        static TocGenerator()
+        {
+            emptyNavDoc.LoadXml(Res.TocXhtmlTemplate);
+        }
+        /// <summary>
         /// Generate a new nav.xhtml document based on the NCX file.
         /// </summary>
         /// <param name="ncxDoc">NCX XmlDocument</param>
         /// <returns>xhtml XmlDocument object</returns>
         internal static XmlDocument Generate(XmlDocument ncxDoc)
         {
+            XmlDocument navDoc = emptyNavDoc.Clone() as XmlDocument;
             return null;
         }
     }
