7d018b40b3c17236e300a967851d13cc040f29fb
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-08-24 16:14:26 +1000

    [filter] Create privacy filter class

---
+++ EpubSanitizer/EpubSanitizerCore/Filters/Privacy.cs
@@ -0,0 +1,15 @@
+﻿namespace EpubSanitizerCore.Filters
+{
+    internal class Privacy(EpubSanitizer CoreInstance) : MultiThreadFilter(CoreInstance)
+    {
+        internal override string[] GetProcessList()
+        {
+            return Utils.PathUtil.GetAllXHTMLFiles(Instance);
+        }
+
+        internal override void Process(string file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
