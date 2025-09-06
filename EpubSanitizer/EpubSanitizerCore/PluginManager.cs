aaa345357f95403eb4f8efab0b2f4887794c8a23
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-09-06 21:20:41 +1000

    [plugin] Improve safety

--- EpubSanitizer/EpubSanitizerCore/PluginManager.cs
+++ EpubSanitizer/EpubSanitizerCore/PluginManager.cs
@@ -3,22 +3,25 @@
 
 namespace EpubSanitizerCore
 {
-
+    /// <summary>
+    /// Basic interface for plugin to implement
+    /// </summary>
     internal abstract class PluginInterface
     {
+        /// <summary>
+        /// OnLoad will be called when plugin is loaded
+        /// </summary>
         internal abstract void OnLoad();
     }
 
     public static class PluginManager
     {
+        /// <summary>
+        /// Supported plugins, keep it same as project InternalsVisibleTo in EpubSanitizerCore.csproj
+        /// </summary>
         public static readonly string[] Plugins = [
             "EpubSanitizerCore.Plugins.DemoPlugin",
         ];
-
-        static PluginManager()
-        {
-
-        }
 
         /// <summary>
         /// Enable all plugins
@@ -34,27 +37,32 @@ namespace EpubSanitizerCore
         /// <param name="pluginName">plugin assembly name</param>
         public static void EnablePlugin(string pluginName)
         {
+            if(!Plugins.Contains(pluginName))
+            {
+                throw new ArgumentException("Plugin not supported: " + pluginName);
+            }
             LoadPlugins([pluginName]);
         }
 
+        /// <summary>
+        /// Load and initialize plugins from list
+        /// </summary>
+        /// <param name="pluginList">string array of plugin assembly name</param>
         private static void LoadPlugins(string[] pluginList)
         {
             //Scan and load plugins
-            string Folder = Path.GetDirectoryName(typeof(PluginManager).Assembly.Location);
+            string Folder = Path.GetDirectoryName(typeof(PluginManager).Assembly.Location) ?? Path.GetDirectoryName(Environment.ProcessPath);
             foreach (string plugin in pluginList)
             {
-                if (File.Exists(Folder + "\\" + plugin + ".dll"))
+                if (File.Exists(Path.Combine(Folder, plugin + ".dll")))
                 {
-                    var plug = Assembly.LoadFrom(Folder + "\\" + plugin + ".dll");
+                    var plug = Assembly.LoadFrom(Path.Combine(Folder, plugin + ".dll"));
                     var pluginEntryType = plug.GetType(plugin + ".PluginEntry");
                     if (pluginEntryType != null && typeof(PluginInterface).IsAssignableFrom(pluginEntryType))
                     {
                         var pluginInstance = (PluginInterface?)Activator.CreateInstance(pluginEntryType);
                         pluginInstance?.OnLoad();
                     }
-
-
-
                 }
             }
         }
