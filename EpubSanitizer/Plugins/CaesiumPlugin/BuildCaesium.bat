c54aeee0cdf0326dd05fa6023b291a4eaf75b49d
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-09-20 21:15:52 +1000

    [plugin-caesium] Fixed dep version

--- EpubSanitizer/Plugins/CaesiumPlugin/BuildCaesium.bat
+++ EpubSanitizer/Plugins/CaesiumPlugin/BuildCaesium.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Build libcaesium with native architecture...
 rem Use native architecture (should be AVX512 on my laptop)
-git clone https://github.com/Lymphatus/libcaesium.git
+git clone -b 0.18.1 https://github.com/Lymphatus/libcaesium.git
 set RUSTFLAGS=-C target-cpu=native -C target-feature=+crt-static
 cd libcaesium
 git rev-list HEAD --count > ..\libcaesium_version.txt
