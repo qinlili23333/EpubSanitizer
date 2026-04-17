e8bdcfb16887a40b7ebb58bcc6df4202ddf9dd76
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2026-04-17 20:58:01 +1000

    [proj] Add MAUI project

---
+++ EpubSanitizer/EpubSanitizerMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EpubSanitizerMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
